f435ed24b9f52e167dcef0f4c36f2c006b5dc33e
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-03 23:25:41 +0330

    update

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+// fonts
+import * as Font from 'expo-font';
 
-// cms
+// cmp
 import Header from './components/Header';
 
 // screen
@@ -9,9 +12,27 @@ import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+};
+
 export default function App() {
   const [usrNumber, setUsrNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+        onError={(err) => console.log("FONT ERROR")}
+      />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -37,7 +58,7 @@ export default function App() {
         roundsNumber={guessRounds}
         userNumber={usrNumber}
         onRestart={configureNewGameHandler}
-      />
+        />
     );
   }
 
