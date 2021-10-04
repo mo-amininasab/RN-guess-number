9442421b6dedcd9dd185aa1c0f4a88ffb51b0154
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-04 12:42:03 +0330

    web image added to GameOverScreen

--- screens/GameOverScreen.jsx
+++ screens/GameOverScreen.jsx
@@ -8,10 +8,20 @@ const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       {/**Local Image */}
-
-      <View style={styles.imageContainer}>
+      {/* <View style={styles.imageContainer}>
         <Image
           source={require('../assets/success.png')}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View> */}
+
+      {/* Web Image */}
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: 'https://cdn.pixabay.com/photo/2019/01/22/18/30/summit-3948706_1280.jpg',
+          }}
           style={styles.image}
           resizeMode="cover"
         />
